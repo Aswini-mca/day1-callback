22de23b498b522daaf1de46d2c754362592cea04
Aswini-mca <135096729+Aswini-mca@users.noreply.github.com>
2023-07-18 15:44:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let timer = 11;
+
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 11000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 10000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 9000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 8000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 7000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 6000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 5000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 4000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 3000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = --timer;
+}, 2000)
+setTimeout(() => {
+    let time = document.getElementById('display');
+    time.style.margin = "auto";
+    time.style.textAlign = "center";
+    time.innerHTML = "Happy Independence Day";
+}, 12000)
